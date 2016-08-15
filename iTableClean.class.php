af94f492c9b356f6a2252892d269f096de8bcd67
Evan Wills <evan.wills@acu.edu.au>
2016-08-15 20:23:06 +1000

    started work on setting up MVC structure

--- iTableClean.class.php
+++ iTableClean.class.php
@@ -1,34 +1,45 @@
 <?php
 
-interface iTableClean {
+interface iTableCleanView {
 	/**
 	 * @function set_classes() specifies user defined classes for individual columns
 	 *
-	 * @param string $element Name of element classes are to be applied to
-	 * @param string, array $classes If string, space separated list of class names to be applied to the element
-	 *                if array one item for each column/row in table (extra items will be ignored). Each item can be either string or boolean
-	 *                if TRUE column/row heading will be used as class name. If FALSE, no class name will be applied to column
+	 * @param string $element Name of element classes are to be
+	 *               applied to
+	 * @param string, array $classes If string, space separated list
+	 *               of class names to be applied to the element. If
+	 *               array one item for each column/row in table
+	 *               (extra items will be ignored). Each item can be
+	 *               either string or boolean. If TRUE column/row
+	 *               heading will be used as class name. If FALSE, no
+	 *               class name will be applied to column
 	 *
 	 * @return void
 	 */
 	public function set_classes($element, $classes);
 
 	/**
-	 * @function get_classes() returns the classes applied to the specified element
+	 * @function get_classes() returns the classes applied to the
+	 *           specified element
 	 *
-	 * @param string $element Name of element classes are to be applied to
-	 * @return string, array $classes space separated list of class names to be applied
+	 * @param string $element Name of element classes are to be
+	 *               applied to
+	 * @return string, array $classes space separated list of class
+	 *               names to be applied
 	 *
 	 * @return void
 	 */
 	public function get_classes($element);
 
 	/**
-	 * @function clean_tables() rewrites the HTML for each table in the input HTML
+	 * @function clean_tables() rewrites the HTML for each table in
+	 *           the input HTML
 	 *
-	 * @param string HTML to be cleaned up
+	 * @param array two dimensional array. First dimension represents
+	 *              rows and the second dimension is the table cells
+	 *              in the row
 	 *
 	 * @return string HTML with clean accessible tables.
 	 */
-	public function clean_tables($html);
+	public function render_table($table_array);
 }
