6f03e6c2886027993d2bd00c0a5f71b17fb502a1
Evan Wills <evan.wills@acu.edu.au>
2016-08-17 11:10:55 +1000

    updated name of class being tested to match actual class name.

--- unit_tests/tableCleanModel.test.php
+++ unit_tests/tableCleanModel.test.php
@@ -11,7 +11,7 @@ class tableCleanModel_test extends PHPUnit_Framework_TestCase {
 	private $sample = 'samples/multi-table.html';
 
 	public function testConstructorAcceptsHTML() {
-		$model = new tableClean(file_get_contents($this->sample));
+		$model = new tableCleanModel(file_get_contents($this->sample));
 
 		$this->assetEquals(24, $model->get_table_count());
 	}
